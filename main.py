85067e8610fc8bfd1652ae93ecccf6365d9e9cc8
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-11-16 15:34:02 +0100

    lokalizacja z ipapi w main.py proste okienko pyqt5 do jej wyświetlania

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 from location import Location
 
-def update():
-    label.setText("Updated")
+class Main:
+
+    def __init__(self):
+        self.location = Location()
+        self.city = self.location.city
+        self.main()
+
+    def main(self):
+        app = QApplication(sys.argv)
+        win = QMainWindow()
+        win.setGeometry(100, 100, 700, 450)
+        win.setWindowTitle("Weather")
+
+        loc_label = QtWidgets.QLabel(win)
+        loc_label.setText(
+            "current location: " + str(self.location.longitude) + "   " + str(self.location.latitude))
+        loc_label.adjustSize()
+        loc_label.move(10, 430)
+
+        current_weather_lebel = QtWidgets.QLabel(win)
+        current_weather_lebel.setText(
+            "current weather for " + str(self.location.city) + ": ")
+        current_weather_lebel.setFont(QFont('Arial', 20))
+        current_weather_lebel.resize(500, 100)
+        current_weather_lebel.move(150, 35)
 
 
-def retrieve():
-    print(label.text())
+        win.show()
+        sys.exit(app.exec_())
 
-location = Location()
-
-app = QApplication(sys.argv)
-win = QMainWindow()
-win.setGeometry(100, 100, 700, 450)
-win.setWindowTitle("Weather")
-
-label = QtWidgets.QLabel(win)
-label.setText("lon: " + str(location.longitude) + "\nlat:" + str(location.latitude) + "\ncity: " + str(location.city))
-label.adjustSize()
-label.move(10, 10)
-
-
-
-win.show()
-sys.exit(app.exec_())
+Main()
