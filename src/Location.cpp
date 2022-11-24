b7e79186a3a58e9dbab024450e4c780214d4e942
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-11-24 23:12:07 +0100

    .

---
+++ src/Location.cpp
@@ -0,0 +1,5 @@
+//
+// Created by witek on 24.11.22.
+//
+
+#include "Location.h"
---
+++ src/Location.h
@@ -0,0 +1,21 @@
+#ifndef WAIO_LOCATION_H
+#define WAIO_LOCATION_H
+
+#include <QGeoPositionInfoSource>
+#include <QApplication>
+#include <QDebug>
+
+
+class Location {
+};
+
+
+using namespace std;
+
+int main() {
+    qDebug() << QT_VERSION_STR;
+    return 1;
+}
+
+
+#endif //WAIO_LOCATION_H
