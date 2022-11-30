54514b33b68d4efa7b77ecb5a2814e2a73d32026
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-11-30 01:13:58 +0100

    .

---
+++ src/weather-api/weather_data_request.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import re
+import subprocess
+
+#lokalizacja dla ktorej pobieramy dane
+lat = 50
+lon = 20
+
+#adres url open meteo api
+API_URL = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relativehumidity_2m,' \
+          'dewpoint_2m,apparent_temperature,precipitation,rain,showers,snowfall,snow_depth,freezinglevel_height,weatherc' \
+          'ode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,visibility,evapotr' \
+          'anspiration,windspeed_10m,windspeed_80m,windgusts_10m,temperature_80m&daily=weathercode,temperature_2m_max,te' \
+          'mperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,' \
+          'showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&timezone=auto'
+
+try:
+    response = requests.post(API_URL)  # wysylanie zapytania
+    loc = response.json()
+    if 'error' in loc.keys():
+        raise requests.RequestException()
+except Exception as e:
+    print(e)
+else:
+    json_object = json.dumps(loc, indent=3)
+    with open("weather_data.json", "w") as outfile:          #wrzucanie odpowiedzi do pliku json
+        outfile.write(json_object)
